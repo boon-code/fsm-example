4f2b43960293061f9d212b49cafa8750a776f49b
Manuel Huber <Manuel.h87@gmail.com>
2022-10-31 19:11:06 +0000

    Add more complex examples

--- fsm_example/src/main.rs
+++ fsm_example/src/main.rs
@@ -4,13 +4,23 @@ use fsm_derive::Fsm;
 enum MyTest {
     Bla,
     Ble,
+    X(u32, u32, i8),
+}
+
+#[derive(Fsm)]
+enum Test2 {
+    A(u32, u32, i8),
+    B,
+    C {name: String, age: u32},
 }
 
 
 fn main() {
     let ble = MyTest::Ble;
     let bla = MyTest::Bla;
+    let x = MyTest::X(5_u32, 7_u32, 7_i8);
     println!("Hello, world! {}", bla.name());
     println!("Hello, world! bla={}", bla.bla());
     println!("Hello, world! ble={}", ble.bla());
+    println!("Hello, world! x={}", x.bla());
 }
